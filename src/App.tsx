437c9a0ad29b0d73a0fd4f54cb2e6faad6f3c326
Igor Stepanov <stipyk1309@gmail.com>
2024-07-29 23:28:01 +0300

    feat: calculate salary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { ChangeEvent, useState } from 'react'
+
 function App() {
-	// const [count, setCount] = useState(0)
+	const [total, setTotal] = useState(0)
+	const REGINAL_COEFFICIENT = 1.65
+	const TAX_PERCENT = 13
+
+	function handleChange(e: ChangeEvent<HTMLInputElement>): void {
+		const value =
+			+e.target.value * REGINAL_COEFFICIENT -
+			(+e.target.value * REGINAL_COEFFICIENT * TAX_PERCENT) / 100
+
+		setTotal(value)
+	}
 
 	return (
 		<main>
 			<h1 className='text-3xl font-bold'>Расчет ЗП</h1>
+			<div className='flex flex-col gap-3 my-5'>
+				<div>
+					<input
+						className='min-h-10 p-2 text-xl font-bold text-black rounded-lg'
+						type='text'
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<span>{total} руб.</span>
+				</div>
+			</div>
 		</main>
 	)
 }
